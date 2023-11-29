70b23651545a15f5a7a1583d313a073663d53e12
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-11-28 23:01:28 -0500

    Update ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,7 +5,10 @@ function ContactPage() {
   return (
     <div className="contact-page">
       <h1>Contact</h1>
-      <p>Welcome to the contact page!</p>
+      <p>If you have any questions or feedback about this project, feel free to reach out to me. As a student dedicated to learning and improving, I welcome your insights and inquiries.</p>
+      <h2>How to Reach Me</h2>
+      <p>You can contact me via my student email for any academic-related queries or project discussions:</p>
+      <a href="mailto:jthom282@students.kennesaw.edu">jthom282@students.kennesaw.edu</a>
     </div>
   );
 }
