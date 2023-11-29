556e63c2e5ee353c60c3def8fc3289ebdcf0784c
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-11-28 23:09:28 -0500

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,10 +9,10 @@ function AboutPage() {
       <h2>Core Features</h2>
       <ul>
         <li><strong>REST API Integration:</strong> Utilizes GET, PUT, POST, and DELETE requests to interact with a cloud-based database, reflecting the CRUD (Create, Read, Update, Delete) operations essential in web applications.</li>
-        <li><strong>Cloud Database Interaction:</strong> Demonstrates how to connect a React frontend with a cloud-hosted database, providing a scalable and flexible data management solution.</li>
+        <li><strong>Cloud Database Interaction:</strong> Demonstrates how to connect a React frontend with a cloud-hosted database.</li>
       </ul>
       <h3>Technologies Used</h3>
-      <p>The app is built using React, with Axios for API requests. It interfaces with a cloud database, showcasing how cloud computing is integrated into frontend development. The styling is achieved using CSS, which is easily customizable to suit different design needs.</p>
+      <p>The app is built using React, with Axios for API requests. It interfaces with a MongoDB, showcasing how cloud computing is integrated into frontend development.</p>
       <h4>Course Assignment Details</h4>
       <p>Assignment for SWE 4633 by Joseph Thompson (11/28/2023), under the guidance of Dr. Xia Li. The project aligns with the learning objectives of Module 6, focusing on cloud platform and REST API interactions.</p>
     </div>
